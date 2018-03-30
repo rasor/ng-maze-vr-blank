26a26548c8b2449a6e1424a5b2730cd2a07597a5
rasor <soren@rasor.dk>
2018-03-30 15:31:18 +0200

    Creating <a-box *ngFor="let b of vrBoxes">

--- src/app/components/aframe-vr/aframe-vr.component.ts
+++ src/app/components/aframe-vr/aframe-vr.component.ts
@@ -1,14 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, 
+  ViewChild, AfterViewInit, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'aframe-vr',
   templateUrl: './aframe-vr.component.html',
   styleUrls: ['./aframe-vr.component.css']
 })
-export class AframeVrComponent implements OnInit {
+export class AframeVrComponent implements OnInit, AfterViewInit  {
+  @ViewChild('scene') theScene: ElementRef;
+  vrBoxes: VrBox[] = [
+    new VrBox()//,
+    //new VrBox()
+  ];
 
   constructor() { }
 
   ngOnInit() {
   }
+
+  ngAfterViewInit(): void {
+    console.log("Found theScene " + this.theScene.nativeElement.tagName);
+  }
+}
+
+class VrBox {
 }
